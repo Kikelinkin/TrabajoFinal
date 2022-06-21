714b24ff81a6758b515bc95a1b09fc1b69e113d4
Kikelinkin <kike_lr1@hotmail.com>
2022-06-21 20:49:28 -0300

    acercade

--- TrabajoFinal/src/app/app.module.ts
+++ TrabajoFinal/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoapComponent } from './components/logoap/logoap.component';
+import { SocialComponent } from './components/social/social.component';
+import { BanerComponent } from './components/baner/baner.component';
+import { AcercadeComponent } from './components/acercade/acercade.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoapComponent,
+    SocialComponent,
+    BanerComponent,
+    AcercadeComponent
   ],
   imports: [
     BrowserModule,
