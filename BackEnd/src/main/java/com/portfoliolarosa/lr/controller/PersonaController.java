d27d02e22be104c41f678b6c0a904ec7f42bb462
Kikelinkin <kike_lr1@hotmail.com>
2022-07-06 19:40:11 -0300

    fin back

---
+++ BackEnd/src/main/java/com/portfoliolarosa/lr/controller/PersonaController.java
@@ -0,0 +1,46 @@
+
+package com.portfoliolarosa.lr.controller;
+
+import com.portfoliolarosa.lr.entty.Persona;
+import com.portfoliolarosa.lr.interfase.IpersonaServices;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonaController {
+    @Autowired IpersonaServices ipersonaServices;
+   @GetMapping("personas/traer")
+    public List<Persona> getPersona(){
+    return ipersonaServices.getPersona();
+    }
+    @PostMapping("/persona/crear")
+    public String createPersona(@RequestBody Persona persona){
+    ipersonaServices.savePersona(persona);
+    return "La Persona fue creada correctamente";
+    }
+    @DeleteMapping("/persona/borrar/(id)")
+    public String deletePersona(@PathVariable Long id){
+    ipersonaServices.deletePersona(id);
+    return "La Persona fue eliminada correctamente";
+    }
+    @PutMapping("/personas/editar/(id)")
+    public Persona editPersona(@PathVariable Long id,
+                               @RequestParam("nombre")String nuevoNombre,
+                               @RequestParam("apellido")String nuevoApellido,
+                               @RequestParam("img")String nuevoImg){
+    Persona persona = ipersonaServices.findPersona(id);
+    persona.setNombre(nuevoNombre);
+    persona.setApellido(nuevoApellido);
+    persona.setImg(nuevoImg);
+    ipersonaServices.savePersona(persona);
+    return persona;
+    }
+}
--- BackEnd/src/main/java/com/portfoliolarosa/lr/service/ImpPersonaService.java
+++ BackEnd/src/main/java/com/portfoliolarosa/lr/service/ImpPersonaService.java
@@ -1,7 +1,37 @@
 
 package com.portfoliolarosa.lr.service;
 
+import com.portfoliolarosa.lr.entty.Persona;
+import com.portfoliolarosa.lr.interfase.IpersonaServices;
+import com.portfoliolarosa.lr.repository.IpersonaRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public class ImpPersonaService {
+@Service
+public class ImpPersonaService implements IpersonaServices{
+@Autowired IpersonaRepository IpersonaRepository;
+    @Override
+    public List<Persona> getPersona() {
+        List<Persona> persona=IpersonaRepository.findAll();
+        return persona;
+    }
+
+    @Override
+    public void savePersona(Persona persona) {
+        IpersonaRepository.save(persona);
+    }
+
+    @Override
+    public void deletePersona(Long id) {
+        IpersonaRepository.deleteById(id);
+    }
+
+    @Override
+    public Persona findPersona(Long id) {
+        Persona persona=IpersonaRepository.findById(id).orElse(null);
+        return persona;
+        
+    }
     
 }
