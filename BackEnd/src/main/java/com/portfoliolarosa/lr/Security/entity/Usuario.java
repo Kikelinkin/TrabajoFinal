bed1e8a848bbeee9785a521d87f1f5ae234581f4
Kikelinkin <kike_lr1@hotmail.com>
2022-07-12 09:26:20 -0300

    ddd

--- BackEnd/src/main/java/com/portfoliolarosa/lr/Security/entity/Usuario.java
+++ BackEnd/src/main/java/com/portfoliolarosa/lr/Security/entity/Usuario.java
@@ -31,4 +31,66 @@ public class Usuario {
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable (name = "usuario_rol",joinColums = @JoinColum (name = "usuario_id"),inverseJoinColumns = @JoinColumn(name = rol_id))
     private Set<Rol> roles = new HashSet <>();
+    //contructortes
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombre, String nombreUsuario, String email, String password) {
+        this.nombre = nombre;
+        this.nombreUsuario = nombreUsuario;
+        this.email = email;
+        this.password = password;
+    }
+    //getter&setter
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Rol> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Rol> roles) {
+        this.roles = roles;
+    }
+    
+    
 }
---
+++ BackEnd/src/main/java/com/portfoliolarosa/lr/Security/entity/UsuarioPrincipal.java
@@ -0,0 +1,7 @@
+
+package com.portfoliolarosa.lr.Security.entity;
+
+
+public class UsuarioPrincipal {
+    
+}
