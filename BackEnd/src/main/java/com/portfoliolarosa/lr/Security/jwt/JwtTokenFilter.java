ebf5f7fdc0529cd8f6ca95fe1628c3283a3747a8
Kikelinkin <kike_lr1@hotmail.com>
2022-07-13 14:25:23 -0300

    Create JwtTokenFilter.java

---
+++ BackEnd/src/main/java/com/portfoliolarosa/lr/Security/jwt/JwtTokenFilter.java
@@ -0,0 +1,13 @@
+
+package com.portfoliolarosa.lr.Security.jwt;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class JwtTokenFilter {
+     private final static Logger logger = LoggerFactory.getLogger(JwtProvider.class);
+     
+     @Autowired
+     JwtProvider jwtprovider;
+}
