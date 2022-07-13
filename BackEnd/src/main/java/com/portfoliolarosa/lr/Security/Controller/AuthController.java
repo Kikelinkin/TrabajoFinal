811f01b31cbbbbbb1dc2621e590305265ced6d20
Kikelinkin <kike_lr1@hotmail.com>
2022-07-13 20:57:11 -0300

    Create AuthController.java

---
+++ BackEnd/src/main/java/com/portfoliolarosa/lr/Security/Controller/AuthController.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfoliolarosa.lr.Security.Controller;
+
+/**
+ *
+ * @author Naty-Kike
+ */
+public class AuthController {
+    
+}
