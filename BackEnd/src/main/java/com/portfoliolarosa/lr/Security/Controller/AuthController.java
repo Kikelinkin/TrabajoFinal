02006f735ed85174dbb1263c53f1bbb3ffca1369
Kikelinkin <kike_lr1@hotmail.com>
2022-07-13 20:57:39 -0300

    Update AuthController.java

--- BackEnd/src/main/java/com/portfoliolarosa/lr/Security/Controller/AuthController.java
+++ BackEnd/src/main/java/com/portfoliolarosa/lr/Security/Controller/AuthController.java
@@ -1,13 +1,64 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.portfoliolarosa.lr.Security.Controller;
 
-/**
- *
- * @author Naty-Kike
- */
+import com.portfoliolarosa.lr.Security.Enums.RolNombre;
+import com.portfoliolarosa.lr.Security.Service.RolService;
+import com.portfoliolarosa.lr.Security.Service.UsuarioService;
+import com.portfoliolarosa.lr.Security.entity.Rol;
+import com.portfoliolarosa.lr.Security.entity.Usuario;
+import com.portfoliolarosa.lr.Security.jwt.JwtProvider;
+import java.util.HashSet;
+import java.util.Set;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin
 public class AuthController {
-    
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    @Autowired
+    AuthenticationManager authenticationManager;
+    @Autowired
+    UsuarioService usuarioService;
+    @Autowired
+    RolService rolService;
+    @Autowired
+    JwtProvider jwtProvider;
+
+    @PostMapping("/nuevo")
+    public ResponseEntity<?> nuevo(@Valid @RequestBody NuevoUsuario nuevoUsuario, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return new ResponseEntity(new Mensaje("Campos mal puestos o email invalido"), HttpStatus.BAD_REQUEST);
+        }
+
+        if (usuarioService.existsByNombreUsuario(nombreUsuario.getNombreUsuario())) {
+            return new ResponseEntity(new Mensaje("Ese nombre de usuario ya existe"), HttpStatus.BAD_REQUEST);
+        }
+
+        if (usuarioService.existsByEmail(nombreUsuario.getEmail())) {
+            return new ResponseEntity(new Mensaje("Ese Email ya existe"), HttpStatus.BAD_REQUEST);
+            Usuario usuario = new Usuario(nuevoUsuario.getNombre(), nuevoUsuario.getNombreUsuario(), nuevoUsuario.getEmail(),passwordEncoder.encode(nuevoUsuario.getPassword()));
+            
+            Set<Rol> = new HashSet <>();
+            roles.add(rolService.getByRolNombre(RolNombre.ROLE_USER).get());
+            if (nuevoUsuario.getRoles().contains("admin"))
+                roles.add(rolService.getByRolNombre(RolNombre.ROLE_ADMIN).get());
+            usuario.setRoles(roles);
+            usuarioService.save(usuario);
+            return new ResponseEntity(new Mensaje("Usuario Guardado"),HttpStatus.CREATED);
+        }
+    public ResponseEntity<JwtDTO> login(@Valid @RequestBody LoginUsuario loginUsuario, BindingResult bindingResult)
+    }
 }
