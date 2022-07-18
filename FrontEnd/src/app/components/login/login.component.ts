28f35ae236ae27c337c64076f11057072db4e4f7
Kikelinkin <kike_lr1@hotmail.com>
2022-07-18 19:45:45 -0300

    Update login.component.ts

--- FrontEnd/src/app/components/login/login.component.ts
+++ FrontEnd/src/app/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginUsuario } from 'src/app/Model/login-usuario';
+import { AuthService } from 'src/app/service/auth.service';
+import { TokenService } from 'src/app/service/token.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+isLogged = false;
+islogginFail = false;
+loginUsuario= LoginUsuario;
+nombreUsuario = "String";
+password!: "String";
+roles: string[] = [];
+errMsg!: string;
 
-  constructor() { }
+
+  constructor(private tokenService: TokenService, private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+    if(this.tokenService.getToken()){
+      this.isLogged = true;
+      this.islogginFail = false;
+      this.roles = this.tokenService.getAuthorities();
+    }
+  }
+
+  onLogin(): void {
+      this.loginUsuario = new LoginUsuario (this.nombreUsuario, this.password, this.authService.login(this.loginUsuario).subscribe(
+        
+      ) )
   }
 
 }
